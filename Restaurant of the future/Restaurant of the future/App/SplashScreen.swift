3aed598e291cc25871e89958839053624e02e615
pavel.khlebnikov <pavel.khlebnikov@rentateam.ru>
2024-06-14 16:45:25 +0300

    add splash screen

---
+++ Restaurant of the future/Restaurant of the future/App/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  Restaurant of the future
+//
+//  Created by Pavel Khlebnikov on 14.06.2024.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
